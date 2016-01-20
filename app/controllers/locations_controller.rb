d6cc3235f5cc7844cb70a837f8e23f6e0d64467e
Derrick Whiting <derrickwhiting@gmail.com>
2016-01-20 15:29:20 -0700

    added crud for locations and biometrics

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -18,4 +18,50 @@
 #
 
 class LocationsController < ApplicationController
+  before_action :find_location, only: [:show, :edit, :destroy, :update]
+  
+  def index
+    @locations = Location.all
+  end
+
+  def show
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @location.update(location_params)
+      redirect_to location_path(@location)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to locations_path
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:lat, :long, :street, :city, :state, :zip, :user_id, :group_id, :workout_id, :exercise_id)
+    end
+
+    def find_location
+      @location = Location.find(params[:id])
+    end
 end
