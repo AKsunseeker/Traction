4b8b639c7178aaf5011a9c8f1977c9804a138352
Ander Rockstad <ander.rockstad@gmail.com>
2016-01-20 15:54:28 -0700

    cruds for workouts and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,53 @@
 
 class UsersController < ApplicationController
   access all: [:index], trainer: :all
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new(user_params)
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to :index
+    else
+      redirect_to :edit
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to user_path
+    else
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :handle)
+  end
+
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -13,4 +13,55 @@
 #
 
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :update, :edit, :destroy]
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new(workout_params)
+  end
+
+  def show
+    @workout = Workout.find(workout_params)
+  end
+
+  def edit
+    
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      redirect_to :new
+    end
+  end
+
+  def update
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    if @workout.destroy
+      redirect_to workout_path
+    else
+      redirect_to workout_path(@workout)
+    end
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :complete)
+  end
+
+  def find_workouts
+    @workout = Workout.find(workout_params)
+  end
 end
