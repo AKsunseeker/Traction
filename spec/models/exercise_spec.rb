19ba9e7819fa697710c82a42ea8b3b2d22af3223
Valeria Roca <valcroca@hotmail.com>
2016-02-03 10:32:55 -0700

    exercise contoller specs

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
   describe 'attributes' do
+    it 'has an id' do 
+      id = 100
+      exercise = Exercise.create(id: id)
+      expect(exercise.id).to eq(id) 
+    end
+
     it 'has a name' do 
       name = 'deadlift'
       exercise = Exercise.create(name: name)
@@ -24,6 +30,12 @@ RSpec.describe Exercise, type: :model do
       repetition_duration_seconds = 1234
       exercise = Exercise.create(repetition_duration_seconds: repetition_duration_seconds)
       expect(exercise.repetition_duration_seconds).to eq(repetition_duration_seconds)
+    end
+
+    it 'has a rest_duration_seconds' do 
+      rest_duration_seconds = 3000
+      exercise = Exercise.create(rest_duration_seconds: rest_duration_seconds)
+      expect(exercise.rest_duration_seconds).to eq(rest_duration_seconds) 
     end
 
     it 'has a complete' do
